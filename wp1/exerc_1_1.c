425551144daa767b010f470656402faf73e5db01
Gustav Skallberg <gskallberg.dev@gmail.com>
2022-01-26 18:27:21 +0100

    Added comments for exerc_1_1

--- wp1/exerc_1_1.c
+++ wp1/exerc_1_1.c
@@ -2,8 +2,12 @@
 #include <stdbool.h>
 int main()
 {
+    
+    // Variable declaration
     int chosenNumber;
     bool exit = false;
+    
+    //Continous loop until the user inputs anything but 1-5
     while (!exit) {
     printf("Please chose a number between 1-5:\n");
     scanf("%d", &chosenNumber);
