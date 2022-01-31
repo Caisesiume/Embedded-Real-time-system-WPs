673a4cef74e3863d4d051877e173231e6239fedc
asiyamohammed <gusasiymo@student.gu.se>
2022-01-31 21:45:48 +0100

    added comments for questions 1 and 3

--- wp1/exerc_1_1.c
+++ wp1/exerc_1_1.c
@@ -2,22 +2,56 @@
 #include <stdbool.h>
 int main()
 {
-    
-    // Variable declaration
+    // Variable declaration for both the inputted value 
+    // and boolean to continue the game or not
     int chosenNumber;
     bool exit = false;
-    
-    //Continous loop until the user inputs anything but 1-5
-    while (!exit) {
-    printf("Please chose a number between 1-5:\n");
-    scanf("%d", &chosenNumber);
-    if(chosenNumber >0 && chosenNumber < 6){
-        printf("This is sentence number %d \n",chosenNumber );
-    }
-    else {
-        //closes the loop
-        exit = true;
-    }
+
+    // Continous loop until the user inputs anything but 1-5
+    while (!exit)
+    {
+        // Message instructing user to enter a number between 1-5
+        printf("Please chose a number between 1-5:\n");
+        // Scan to read users input
+        scanf("%d", &chosenNumber);
+
+        // Condition if the user inputs the number 1
+        // Prints out the sentence intended
+        if (chosenNumber == 1)
+        {
+            printf("The mother read a story to her son.\n");
+        }
+        // Condition if the user inputs the number 2
+        // Prints out the sentence intended
+        else if (chosenNumber == 2)
+        {
+            printf("Tom made a big donation to the hospital.\n");
+        }
+        // Condition if the user inputs the number 3
+        // Prints out the sentence intended
+        else if (chosenNumber == 3)
+        {
+            printf("Diamonds are forever.\n");
+        }
+        // Condition if the user inputs the number 4
+        // Prints out the sentence intended
+        else if (chosenNumber == 4)
+        {
+            printf("She had a dice with over twenty sides.\n");
+        }
+        // Condition if the user inputs the number 5
+        // Prints out the sentence intended
+        else if (chosenNumber == 5)
+        {
+            printf("The horse raced past the barn and fell.\n");
+        }
+        // If the user inputs any number outside of numbers between 1-5 
+        // the program will end
+        else
+        {
+            //closes the loop
+            exit = true;
+        }
     }
     return 0;
 }
--- wp1/exerc_1_3.c
+++ wp1/exerc_1_3.c
@@ -4,19 +4,110 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-#define CHANCES 2 // NUMBER OF CHANCES USER HAS
+#define CHANCES 2 // Defining the number of chances a user has
 
+// ------ Function declarations   ----------
+
+// this function used to start playing the game
 int guessGame();
+
+// ------ Function definitions   ---------- 
+
+
+// Calling the function for the game to start
+int guessGame()
+{
+  // Makes sure the random number selected in each round changes
+  srand(time(NULL));
+  // Declaring random number and stating the minimum and max numbers to randomize between
+  // Formula is rand % (MAX number + MIN number - 1)
+  int randomNumber = rand() % (100 + 1 - 1);
+  // Initializing variable to take in the number chosen by the user
+  int chosenNumber;
+  // Number of tries the user makes
+  int i;
+  // Variable used to inspect whether user inputted a character
+  int charinput;
+  // Message printed to user introducing the game and how many tries the user has
+  printf("Welcome to the guess game! A number has been chosen between 1-100.");
+  printf("You have %d chances!\n", CHANCES);
+
+  // Loop starting the game and i is initialized at 0 to number of tries user has - 1
+  for (i = 0; i < CHANCES; i++)
+  {
+    // Once the round starts the user should see the message to enter a number between 1-100
+    printf("Please enter your guess between 1-100:\n");
+
+    // This loop checks for if the user has inputted a character
+    // if the scan results in a 0 or false this loop will be entered until
+    // the scanner finds an integer or results in 1 or true
+    while (scanf("%d", &chosenNumber) != 1)
+    {
+      // If the input entered has no spaces the loop should be entered
+      // getchar() should retrieve the input and convert it to integers
+      while (!isspace(charinput = getchar()))
+        ;
+      // This function should but the integer created back into the input stream
+      ungetc(charinput, stdin);
+      // Message letting the user know to enter a valid number
+      printf("You must enter a valid number. Try again:\n");
+      //
+      continue;
+    }
+    // If the user input was a number over 100 or less than 1
+    // it should not count as a round and should instead let user to try guessing
+    // with a valid number between 1-100
+    if (chosenNumber > 100 || chosenNumber < 1)
+    {
+      i--;
+      printf("Invalid Number, try again with a number between 1-100:\n");
+    }
+    // If the user guessed the right number the should a message
+    // telling them they chose the right number and how many tries it took
+    else if (chosenNumber == randomNumber)
+    {
+      printf("You guessed correctly and have guessed %d times\n ", CHANCES);
+      break;
+    }
+    // If the user inputted a number greater than the random number
+    // the user should see a message telling the number was too high
+    else if (chosenNumber > randomNumber)
+    {
+      printf("You guessed too high\n");
+    }
+    // If the user inputted a number lesser than the random number
+    // the user should see a message telling the number was too low
+    else if (chosenNumber < randomNumber)
+    {
+      printf("You guessed too low \n");
+    }
+  }
+  // If the tries are all used up the user should a message saying game is over
+  if (i == CHANCES)
+  {
+    printf("Game Over\n");
+  }
+  // The the game should end at this point
+  return 0;
+}
+
+
+// ------ Main   -------------------------- 
 
 int main()
 {
-
+  // Variable to store users answer to play again
   int answer;
+  // Continous loop to play the game until user inputs the number 2
   while (1)
   {
+    // Calling the function to start the game
     guessGame();
-    printf("Do you want to play another round? \n Type 1 for Yes and 2 for No\n");
+    // Printing the question to play again once game is over or player wins the game
+    printf("Do you want to play another round? Type 1 for Yes and 2 for No\n");
+    // Scanning the inputs answer to play again
     scanf("%d", &answer);
+    // Condition for if the users answer is 2 to end the game
     if (answer == 2)
     {
       return 0;
@@ -25,50 +116,3 @@ int main()
   return 0;
 }
 
-int guessGame()
-{
-  srand(time(NULL));
-  int randomNumber = rand() % (100 + 1 - 1);
-  int chosenNumber;
-  int i; //number of tries
-  int charinput;
-  printf("Welcome to the guess game! A number has been guessed between.");
-  printf("You have %d chances!\n", CHANCES);
-
-  for (i = 0; i < CHANCES; i++)
-  {
-    printf("Please enter you guess between 1-100:");
-
-    while (scanf("%d", &chosenNumber) != 1)
-    {
-      while (!isspace(charinput = getchar()))
-        ;
-      ungetc(charinput, stdin);
-      printf("You must enter a valid number. Try again.\n");
-      continue;
-    }
-    if ((chosenNumber > 100 || chosenNumber < 1 || isalpha(chosenNumber) != 0))
-    {
-      i--;
-      printf("Invalid Number, try again:\n");
-    }
-    else if (chosenNumber == randomNumber)
-    {
-      printf("You guessed correctly and have guessed %d times\n ", CHANCES);
-      break;
-    }
-    else if (chosenNumber > randomNumber && i != CHANCES - 1)
-    {
-      printf("You guessed too high\n");
-    }
-    else if (chosenNumber < randomNumber && i != CHANCES - 1)
-    {
-      printf("You guessed too low \n");
-    }
-  }
-  if (i == CHANCES)
-  {
-    printf("Game Over\n");
-  }
-  return 0;
-}
