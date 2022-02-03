96b3b0ede1da7cdf366ea25ac40c81a9ae1dfceb
Gustav Skallberg <gustav.skallberg@gmail.com>
2022-02-03 17:46:01 +0100

    Can manually register people

--- wp2/exerc_2_3.c
+++ wp2/exerc_2_3.c
@@ -1,13 +1,47 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include<stdlib.h>
+#include<string.h>
 
 #define MAX 20
+ 
+ 
+// -----typedefs ------- 
+typedef struct { 
+        char firstname[20]; 
+        char famname[20]; 
+        char pers_number[13]; // yyyymmddnnnc 
+} PERSON; 
+ 
+// Function declaration (to be extend)  
+PERSON input_record(void);             // Reads a person’s record. 
+void write_new_file(PERSON *inrecord);  // Creates a file and  
+// writes the first record 
+void printfile(void);     // Prints out all persons in the file 
+void search_by_firstname(char *name); // Prints out the person if    
+// in list  
+void append_file(PERSON *inrecord);    // appends a new person to the file 
+ 
 
-// Provides the menu for the program and asks the user which functions
-// They want to use and directs them there
-void menu();
+void writeNewFile(PERSON *inrecord) {
 
-void menu() {
+     
+    
+    
+    FILE* fileToWrite;
+    char *fileName = "C:/Users/gusta/coding/SEM/DIT632-WPs/TesAat.txt";
+    fileToWrite = fopen(fileName, "w");
+    fprintf(fileToWrite, inrecord->firstname);
+    fprintf(fileToWrite, inrecord->famname);
+    fprintf(fileToWrite, inrecord->pers_number);
+    fclose(fileToWrite);
+
+
+
+}
+
+int main(void){ 
+    PERSON ppost;    
     
     bool exit = false;
     int choice;
@@ -23,6 +57,29 @@ void menu() {
     
         scanf("%d", &choice);
 
+        if(choice == 1) {
+
+            strncpy(ppost.firstname, "Putte ", 20);
+            strncpy(ppost.famname, "sundell ", 20);
+            strncpy(ppost.pers_number, "1010101", 13);
+
+            writeNewFile(&ppost);
+        }
+        
+        if(choice == 2) {
+
+            printf("Enter the name of the person you want to add\n");
+            scanf("%s", ppost.firstname);
+            printf("Now enter the surname\n");
+            scanf("%s", ppost.famname);
+            printf("Lastly enter the persons personnumber (yyyymmddxxxx)\n");
+            scanf("%s", ppost.pers_number);
+            
+            writeNewFile(&ppost);
+
+            printf("entry successfully added!\n");
+        }
+
         if(choice == 5) {
 
             printf("Bye Bye\n");
@@ -30,10 +87,8 @@ void menu() {
         }
 
     }
-}
 
-int main() {
+    return(0);     
+} 
 
-    menu();
-    return 0;
-}
+    
