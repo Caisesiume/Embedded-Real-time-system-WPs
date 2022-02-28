8ba8f13d591c24ae74f2986738a2fb109e595a11
Gustav <gustav.skallberg@gmail.com>
2022-02-28 16:46:12 +0100

    Added comments and cleaned up some code

--- wp6/exerc_6.2.c
+++ wp6/exerc_6.2.c
@@ -1,3 +1,5 @@
+// Defintions for the different pins
+
 #define ECHOPIN 12
 #define TRIGPIN 11
 #define BUZZER 10
@@ -6,42 +8,61 @@
 #define LED3 7
 #define LED4 8
 
+// Tresh hold for the distance when the lights will start blinking
 int blinkThreshHold = 25;
+
+// The variable that the functions will use as a parameter
 int distance;
+// Distance in centimeters to indicate change in tone and lights lit
 int threshHold[] = {200, 120, 80, 30};
+// Intervals for sound
 int soundVoltage[] = {100, 300, 600, 1000};
+
+// array for deciding how many LEDS to should lit up in the lightLED function
 int pinDecider[] = {LED1, LED2, LED3, LED4};
 
 void setup() {
-pinMode(TRIGPIN, OUTPUT); // Sets the trigPin as an Output
-pinMode(ECHOPIN, INPUT); // Sets the echoPin as an Input
+
+// Initialising all the pins
+pinMode(ECHOPIN, INPUT); 
+pinMode(TRIGPIN, OUTPUT); 
 pinMode(BUZZER, OUTPUT);
 pinMode(LED1, OUTPUT);
 pinMode(LED2, OUTPUT);
 pinMode(LED2, OUTPUT);
 pinMode(LED3, OUTPUT);
 pinMode(LED4, OUTPUT);
-Serial.begin(9600); // Starts the serial communication
+
+// Sets the serial
+Serial.begin(9600);
 
 }
 
 void loop() {
 
+  // Gets the distance  
   distance = getDistance();
+  // Starts the buzzer
   buzzAlert(distance);
+
+  // Makes all the lights blink if it is below the blinkThreshhold
   if (distance < blinkThreshHold){
     blink();
   }
+  // If not lighLED will decide how many will be lit up depending on distance
   else {
     lightLED(distance);
   }
 
 }
 
+
 int getDistance() {
-    // Clears the trigPin
+// CODE TAKEN FROM https://randomnerdtutorials.com/complete-guide-for-ultrasonic-sensor-hc-sr04/
+// to measure the distance with the ultrasonic sensor
 int cm;
 int duration;
+// Clears the trigPin
 digitalWrite(TRIGPIN, LOW);
 delayMicroseconds(2);
 // Sets the trigPin on HIGH state for 10 micro seconds
@@ -54,7 +75,7 @@ duration = pulseIn(ECHOPIN, HIGH);
 cm = duration*0.034/2;
 // Prints the distance on the Serial Monitor
 Serial.print("Distance from the object = ");
-Serial.print(distance);
+Serial.print(cm);
 Serial.println(" cm");
 delay(500);
 
@@ -62,6 +83,7 @@ return cm;
 
 }
 
+// Function to determine how intense the sound will be depending on distance to the object
 void buzzAlert(int distance) {
 
     if( distance < threshHold[3]&& distance >= 0 ) {
@@ -88,44 +110,42 @@ void buzzAlert(int distance) {
     }
 
 }
-
+// Lights up the amount of LEDs depending on the distance to the object
 void lightLED(int distance) {
 
+    // Checks through all the lights and lights them accordingly
+    // loops 4 times since there are 4 LEDs
     for (int i = 0; i < 4; i++) {
 
+        // If distance is lower than the corresponding threshhold it will light the LED
         if(distance < threshHold[i]) {
             digitalWrite(pinDecider[i], HIGH);
 
         }
-
+        // if not it will turn it off
         else {
             digitalWrite(pinDecider[i], LOW);
 
             }
         }
     }
+
+// Turns on all the lights and then turns them off after a small delay   
 void blink() {
 
-    light();
-    delay(250);
-    darken();
-    delay(100);
-
-}
-
-void light() {
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, HIGH);
     digitalWrite(LED4, HIGH);
-}
+    delay(250);
 
-void darken() {
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
+    delay(100);
 
 }
+
 
 
